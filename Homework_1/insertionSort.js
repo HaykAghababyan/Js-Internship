844f921a2cd3ce3038df29ce303d891e924fc5b2
Hayk <hayk.ag942@gmail.com>
2019-03-17 19:49:39 +0400

    add insertionSort

---
+++ Homework_1/insertionSort.js
@@ -0,0 +1,18 @@
+const insertionSort = arr => {
+   for(let i = 0; i < arr.length - 1; i++) {
+     
+      if(arr[i] > arr[i+1]) {
+
+         for(let j = i + 1; j > 0; j--){
+               
+               if(arr[j] < arr[j-1]) {
+
+                  [arr[j], arr[j-1]] = [arr[j-1],arr[j]];
+               }
+               else break;
+         }
+      }
+   }         
+   return arr;
+}
+console.log(insertionSort([125,7,54,23,45,74,100,96,76]))
